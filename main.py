eafe349906d2830964c5e8f1f2e0b2b15e108404
Valentyn Rosputnii <rosputnyv@gmail.com>
2020-02-19 23:17:39 +0200

    initial

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	name = request.args.get("name", "World")
-	return f'Rosputnii Valentyn TZ-72'
+   name = request.args.get("name", "World")
+   return f'Rosputnii Valentyn TZ-72'
 
 @app.route('/first')
-def try():
-	a = None
-	b = 123
-	c = 'It is me'
+def helllo():
+   a = None
+   b = 123
+	c = 'fghjk'
 	return f'{a}, {b}, {c}'
 
 if __name__ == '__main__':
