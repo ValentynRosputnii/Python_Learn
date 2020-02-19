cfd193371823eaf4a94d5ccf6d03efd570b374cc
Valentyn Rosputnii <rosputnyv@gmail.com>
2020-02-19 22:53:48 +0200

    initial

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ def hello():
 	name = request.args.get("name", "World")
 	return f'Rosputnii Valentyn TZ-72'
 
+@app.route('/first/)
+def try():
+	a = None
+	b = 123
+	c = 'It is me'
+	return f'{a}, {b}, {c}'
+
 if __name__ == '__main__':
 	app.run('0.0.0.0')
