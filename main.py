dbef03a83f8eb3079192d551ecc5bb3f52bf3c9e
Valentyn Rosputnii <rosputnyv@gmail.com>
2020-02-19 23:18:29 +0200

    initial

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-   name = request.args.get("name", "World")
-   return f'Rosputnii Valentyn TZ-72'
+	name = request.args.get("name", "World")
+	return f'Rosputnii Valentyn TZ-72'
 
 @app.route('/first')
 def helllo():
-   a = None
-   b = 123
+	a = None
+	b = 123
 	c = 'fghjk'
 	return f'{a}, {b}, {c}'
 
